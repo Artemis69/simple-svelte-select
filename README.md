04b0add96ac7e495bda2496e2409aba14916037d
Artemis <44583679+Artemis69@users.noreply.github.com>
2020-12-29 15:17:27 +0300

    update

--- README.md
+++ README.md
@@ -14,7 +14,11 @@ npm install simple-svelte-select
 
 ## Repl
 
-https://svelte.dev/repl/d8af37de112a45f3a3da2ffee0e7eea7?version=3.31.0
+[Simple](https://svelte.dev/repl/d8af37de112a45f3a3da2ffee0e7eea7?version=3.31.0 "Svelte Repl")
+
+In this repl you can see the scrolling
+
+[Simple but a little more items](https://svelte.dev/repl/a7ae80f2cac54bed810f576fe2655b06?version=3.31.0 "Svelte Repl")
 
 ## Simple example
 
@@ -32,5 +36,74 @@ let selectedValue = items[0];
 
 <Select items={items} bind:selectedValue={selectedValue} placeholder="Select apple or lemon"/>
 ````
+## Setting up
 
-Thats all
+By default Select prohibits entering a search query on mobile devices (If the device width is less than 640px). You can pass the disabled property to control this yourself
+
+````jsx
+//This Select does not allow you to search
+<Select items bind:selectedValue disabled/>
+//This default Select allows you to search if your device (or browser window) is wider than 640 pixels
+<Select items bind:selectedValue/>
+//This select allows you to search
+<Select items bind:selectedValue disabled={false}/>
+````
+
+The items array should not be empty, and should be as in the example above.
+
+selectedValue must be undefined or items[i].
+In the example above, selectedValue is items[0] which is Tasty Apple.
+
+## Styling
+
+You can change the coloring of Select using CSS variables.
+By default, Select includes a light and a dark theme.
+Read more about css variables [here](https://developer.mozilla.org/ru/docs/Web/CSS/Using_CSS_custom_properties "MDN Web Docs").
+You just have to specify the variable and its value. Use "!important" to make the colors apply.
+Take a look at the example below:
+````css
+:root{
+/* Background color */
+--simple-svelte-select-background-color: #fff !important;
+/* Select active border color */
+--simple-svelte-select-container-active-color: #006fe8 !important;
+/* Select border color */
+--simple-svelte-select-border-color: #d8dbdf !important;
+/* Select border color hover */
+--simple-svelte-select-border-hover-color: #b2b8bf !important;
+/* Item background color; Selected item in a list */
+--simple-svelte-select-item-state-active-color: #007bff !important;
+/* Item text color; Selected item in a list */
+--simple-svelte-select-item-active-text-color: #fff !important;
+/* Items shadow */
+ --simple-svelte-select-items-shadow: 0 2px 3px 0 rgba(44, 62, 80, 0.24) !important;
+/* Each item :hover color */
+--simple-svelte-select-item-hover-color: #E7F2FF !important;
+/* Each item :active color */
+--simple-svelte-select-item-active-color: #e2efff !important;
+/* Input text color */
+--simple-svelte-select-input-text-color: #3f4f5f !important;
+/* Input placeholder color */
+--simple-svelte-select-input-placeholder-color: #333 !important;
+/* Selected value color */
+--simple-svelte-select-selected-color: #000 !important;
+/* Item text color (not selected) */
+--simple-svelte-select-item-text-color: #000 !important;
+}
+````
+Go to [example](https://svelte.dev/repl/1bca66aa2644426e83a1381a117d9fe4?version=3 "Svelte Repl") for more insights
+
+Using the media query you can set up a dark theme
+````css
+@media (prefers-color-scheme: dark) {
+	 :root {
+		/* HERE */
+	}
+}
+````
+
+If you dont like new theming you can install 1.0.13 version
+
+````bash
+yarn add simple-svelte-select@1.0.13
+````
--- src/Select.svelte
+++ src/Select.svelte
@@ -4,6 +4,8 @@
     export let items = undefined;
     export let placeholder = 'Select...';
     export let selectedValue = undefined;
+
+	export let disabled = window.matchMedia('(max-width: 640px)').matches;
   
     let isActive = false;
   
@@ -33,269 +35,279 @@
             items[i].hidden = false;
         }
     }
-  </script>
+
+</script>
   
-  {#if items.length != 0}
-      <div class="selectContainer" class:active={isActive} use:clickOutside on:click_outside={() => {isActive = false}}>
-          {#if selectedValue}
-              <div class="selectedValue" on:click={() => isActive = !isActive}>
-                  <span>
-                      {selectedValue.label}
-                  </span>
-                  <span class="remove" on:click={() => {selectedValue = undefined; isActive = false; clearSearch();}}>
-                      &times;
-                  </span>
-              </div>
-          {/if}
-          <div class="inputContainer" on:click={() => isActive = !isActive}>
-              <input 
-                  type="text" 
-                  placeholder={placeholder} 
-                  autocomplete="off" 
-                  autocorrect="off" 
-                  spellcheck="false"
-                  bind:value={value}
-                                  on:input={inputHandler}
-              >
-          </div>
-          {#if isActive}
-              <div class="items">
-                  {#each items as item, i}
-                        {#if !item.hidden}
-                            <div class="itemContainer" class:active="{selectedValue == items[i]}">
-                                <div class="item" on:click={() => {isActive = false; selectedValue=items[i]; clearSearch();}}>
-                                    <span>{item.label}</span>
-                                </div>
+{#if items.length != 0}
+    <div class="selectContainer" class:active={isActive} use:clickOutside on:click_outside={() => {isActive = false}}>
+        {#if selectedValue}
+            <div class="selectedValue" on:click={() => isActive = !isActive}>
+                <span>
+                    {selectedValue.label}
+                </span>
+                <span class="remove" on:click={() => {selectedValue = undefined; isActive = false; clearSearch();}}>
+                    &times;
+                </span>
+            </div>
+        {/if}
+        <div class="inputContainer" on:click={() => isActive = !isActive}>
+            <input 
+                type="text" 
+                placeholder={placeholder} 
+                autocomplete="off" 
+                autocorrect="off" 
+				spellcheck="false"
+				disabled={disabled}
+                bind:value={value}
+                on:input={inputHandler}
+            >
+        </div>
+        {#if isActive}
+            <div class="items">
+                {#each items as item, i}
+                    {#if !item.hidden}
+                        <div class="itemContainer" class:active="{selectedValue == items[i]}">
+                            <div class="item" on:click={() => {isActive = false; selectedValue=items[i]; clearSearch();}}>
+                                <span>{item.label}</span>
                             </div>
-                        {/if}
-                  {/each}
-              </div>
-          {/if}
+                        </div>
+                    {/if}
+                {/each}
+            </div>
+        {/if}
       </div>
-  {/if}
+{/if}
   
-  <style>
-      .selectContainer{
-          background: #fff;
-          border: 1px solid #d8dbdf;
-          border-radius: 3px;
-          padding: 0 16px;
-          position: relative;
-          display: -webkit-box;
-          display: flex;
-          -webkit-box-align: center;
-          align-items: center;
-          height: 42px;
-          -webkit-user-select: none;
-             -moz-user-select: none;
-                  user-select: none;
-          width: 100%;
-          min-width: 152px;
-      }
-      .selectContainer:hover{
-          border-color: #b2b8bf;
-      }
-      .selectContainer.active{
-          border-color: #006fe8;
-          outline: none;
-      }
-      .selectedValue{
-              border-radius: 3px;
-          background: #fff;
-          position: absolute;
-          height: 100%;
-          left: 0;
-          right: 0;
-          display: -webkit-box;
-          display: flex;
-          -webkit-box-pack: justify;
-                  justify-content: space-between;
-          -webkit-box-align: center;
-                  align-items: center;
-          z-index: 3;
-          line-height: 42px;
-      }
-      .selectedValue > *{
-          margin: 0 16px;
-      }
-      .inputContainer{
-              border-radius: 3px;
-          position: absolute;
-          height: 100%;
-          left: 0;
-          right: 0;
-          display: -webkit-box;
-          display: flex;
-          -webkit-box-pack: center;
-                  justify-content: center;
-          -webkit-box-align: center;
-                  align-items: center;
-          background: #fff;
-          z-index: 2;
-      }
-      .inputContainer input{
-              border-radius: 3px;
-          font-size: 1em;
-          color: #3f4f5f;
-          padding: 0;
-          line-height: 42px;
-          height: 100%;
-          letter-spacing: -0.08px;
-          cursor: default;
-          width: calc(100% - 32px);
-          border: none;
-          margin: 0;
-      }
-      .inputContainer input:focus{
-          outline: none;
-      }
-      .items{
-          position: absolute;
-          left: 0;
-          right: 0;
-          top: 48px;
-          background-color: #fff;
-          border-radius: 4px;
-          -webkit-box-shadow: 0 2px 3px 0 rgba(44, 62, 80, 0.24);
-                  box-shadow: 0 2px 3px 0 rgba(44, 62, 80, 0.24);
-          overflow-y: auto;
-          z-index: 4;
-          max-height: 250px;
-      }
-      .itemContainer{
-          width: 100%;
-          height: 42px;
-          background: #fff;
-          display: -webkit-box;
-          display: flex;
-          -webkit-box-pack: center;
-                  justify-content: center;
-          -webkit-box-align: center;
-                  align-items: center;
-      }
-      .itemContainer:hover{
-          background: #E7F2FF;
-      }
-      .itemContainer:active{
-          background: #e2efff;
-      }
-      .item{
-          width: calc(100% - 32px);
-          height: 42px;
-          display: -webkit-box;
-          display: -ms-flexbox;
-          display: flex;
-          -webkit-box-pack: start;
-                  justify-content: flex-start;
-          -webkit-box-align: center;
-                  align-items: center;
-      }
-      .item span{
-          font-size: 1em;
-      }
-      .remove{
-          cursor: pointer;
-          margin-top: -4px;
-          font-size: 1.5em;
-      }
-      .itemContainer.active{
-          background: #007bff;
-          color: #fff;
-      }
-      input:disabled {
-          background: #fff;
-      }
-  @media (prefers-color-scheme: dark) {
-      .selectContainer{
-          background: #374151;
-          border: 1px solid #4B5563;
-      }
-      .selectContainer:hover{
-          border-color: #535e6e;
-      }
-      .selectContainer.active:hover{
-          border-color: #006fe8;
-      }
-      .selectedValue{
-          background: #374151;
-      }
-      .inputContainer{
-          background: #374151;
-      }
-      .itemContainer{
-          background: #374151;
-      }
-      .itemContainer:hover{
-          background: #4B5563;
-      }
-      .itemContainer:active{
-          background: #535f6e;
-      }
-      .items{
-          background: #374151;
-      }
-      .inputContainer input{
-          background: #374151;
-          color: #eee;
-      }
-      input:disabled {
-          background: #374151;
-      }
-      .inputContainer input::-webkit-input-placeholder {
-          color: #ddd;
-      }
-      .inputContainer input::-moz-placeholder {
-          color: #ddd;
-      }
-      .inputContainer input:-ms-input-placeholder {
-          color: #ddd;
-      }
-      .inputContainer input::-ms-input-placeholder {
-          color: #ddd;
-      }
-      .inputContainer input::placeholder {
-          color: #ddd;
-      }
-      .item span{
-          color: #eee;
-      }
-      .selectContainer *{
-          color: #eee;
-      }
-      .itemContainer.active{
-          background: #6B7280;
-      }
-      .itemContainer.active .item span{
-          color: #eee;
-      }
-  }
-  ::-webkit-scrollbar {
-      width: 8px;
-      height: 8px;
-      background-color: rgba(0,0,0,0);
-      -webkit-border-radius: 100px;
-  }
-  
-  ::-webkit-scrollbar:hover {
-      background-color: rgba(0, 0, 0, 0.09);
-  }
-  
-  ::-webkit-scrollbar-thumb {
-      background: rgba(0,0,0,0.5);
-      -webkit-border-radius: 100px;
-  }
-  ::-webkit-scrollbar-thumb:active {
-      background: rgba(0,0,0,0.61); 
-      -webkit-border-radius: 100px;
-  }
-  
-  ::-webkit-scrollbar-thumb:vertical {
-    min-height: 10px;
-  }
-  ::-webkit-scrollbar-thumb:horizontal {
-    min-width: 10px;
-  }
-  ::-webkit-resizer{
-      background-color:transparent;
-  }
-  </style>
+<style>
+:root {
+	--simple-svelte-select-container-active-color: #006fe8;
+	--simple-svelte-select-item-state-active-color: #007bff;
+	--simple-svelte-select-item-active-text-color: #fff;
+	--simple-svelte-select-background-color: #fff;
+	--simple-svelte-select-border-color: #d8dbdf;
+	--simple-svelte-select-border-hover-color: #b2b8bf;
+	--simple-svelte-select-items-shadow: 0 2px 3px 0 rgba(44, 62, 80, 0.24);
+	--simple-svelte-select-item-hover-color: #E7F2FF;
+	--simple-svelte-select-item-active-color: #e2efff;
+	--simple-svelte-select-input-text-color: #3f4f5f;
+	--simple-svelte-select-input-placeholder-color: #333;
+	--simple-svelte-select-selected-color: #000;
+	--simple-svelte-select-item-text-color: #000;
+}
+
+@media (prefers-color-scheme: dark) {
+	 :root {
+		--simple-svelte-select-container-active-color: #006fe8;
+		--simple-svelte-select-item-state-active-color: #414141;
+		--simple-svelte-select-item-active-text-color: #fff;
+		--simple-svelte-select-background-color: #161616;
+		--simple-svelte-select-border-color: #333;
+		--simple-svelte-select-border-hover-color: #444;
+		--simple-svelte-select-item-hover-color: #252525;
+		--simple-svelte-select-item-active-color: #323232;
+		--simple-svelte-select-input-text-color: #eee;
+		--simple-svelte-select-input-placeholder-color: #ddd;
+		--simple-svelte-select-selected-color: #eee;
+		--simple-svelte-select-item-text-color: #eee;
+	}
+}
+
+.selectContainer {
+	background: var(--simple-svelte-select-background-color);
+	border: 1px solid var(--simple-svelte-select-border-color);
+	border-radius: 3px;
+	padding: 0 16px;
+	position: relative;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-align: center;
+	align-items: center;
+	height: 42px;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	user-select: none;
+	width: 100%;
+	min-width: 152px;
+}
+
+.selectContainer:hover {
+	border-color: var(--simple-svelte-select-border-hover-color);
+}
+
+.selectContainer.active {
+	border-color: var(--simple-svelte-select-container-active-color);
+	outline: none;
+}
+
+.selectedValue {
+	border-radius: 3px;
+	background: var(--simple-svelte-select-background-color);
+	position: absolute;
+	height: 100%;
+	left: 0;
+	right: 0;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-pack: justify;
+	justify-content: space-between;
+	-webkit-box-align: center;
+	align-items: center;
+	z-index: 3;
+	line-height: 42px;
+}
+.selectedValue span{
+	color: var(--simple-svelte-select-selected-color);
+}
+
+.selectedValue>* {
+	margin: 0 16px;
+}
+
+.inputContainer {
+	border-radius: 3px;
+	position: absolute;
+	height: 100%;
+	left: 0;
+	right: 0;
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-pack: center;
+	justify-content: center;
+	-webkit-box-align: center;
+	align-items: center;
+	background: var(--simple-svelte-select-background-color);
+	z-index: 2;
+}
+
+.inputContainer input {
+	background: transparent;
+	border-radius: 3px;
+	font-size: 1em;
+	color: var(--simple-svelte-select-input-text-color);
+	padding: 0;
+	line-height: 42px;
+	height: 100%;
+	letter-spacing: -0.08px;
+	cursor: default;
+	width: calc(100% - 32px);
+	border: none;
+	margin: 0;
+}
+
+.inputContainer input:focus {
+	outline: none;
+}
+
+.items {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 48px;
+	background-color: var(--simple-svelte-select-background-color);
+	border-radius: 4px;
+	-webkit-box-shadow: var(--simple-svelte-select-items-shadow);
+	box-shadow: var(--simple-svelte-select-items-shadow);
+	overflow-y: auto;
+	z-index: 4;
+	max-height: 250px;
+}
+
+.itemContainer {
+	width: 100%;
+	height: 42px;
+	background: var(--simple-svelte-select-background-color);
+	display: -webkit-box;
+	display: flex;
+	-webkit-box-pack: center;
+	justify-content: center;
+	-webkit-box-align: center;
+	align-items: center;
+}
+
+.itemContainer:hover {
+	background: var(--simple-svelte-select-item-hover-color);
+}
+
+.itemContainer:active {
+	background: var(--simple-svelte-select-item-active-color);
+}
+
+.item {
+	width: calc(100% - 32px);
+	height: 42px;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-pack: start;
+	justify-content: flex-start;
+	-webkit-box-align: center;
+	align-items: center;
+}
+
+.item span {
+	font-size: 1em;
+}
+
+.itemContainer:not(.active) .item span{
+	color: var(--simple-svelte-select-item-text-color);
+}
+
+.remove {
+	cursor: pointer;
+	font-size: 1.5em;
+    margin-top: -4px !important;
+}
+
+.itemContainer.active {
+	background: var(--simple-svelte-select-item-state-active-color);
+	color: var(--simple-svelte-select-item-active-text-color);
+}
+
+.inputContainer input::-webkit-input-placeholder {
+	color: var(--simple-svelte-select-input-placeholder-color);
+}
+
+.inputContainer input::-moz-placeholder {
+	color: var(--simple-svelte-select-input-placeholder-color);
+}
+
+.inputContainer input::placeholder {
+	color: var(--simple-svelte-select-input-placeholder-color);
+}
+
+
+/* Scroll */
+
+ ::-webkit-scrollbar {
+	width: 8px;
+	height: 8px;
+	background-color: rgba(0, 0, 0, 0);
+	-webkit-border-radius: 100px;
+}
+
+ ::-webkit-scrollbar:hover {
+	background-color: rgba(0, 0, 0, 0.09);
+}
+
+ ::-webkit-scrollbar-thumb {
+	background: rgba(0, 0, 0, 0.5);
+	-webkit-border-radius: 100px;
+}
+
+ ::-webkit-scrollbar-thumb:active {
+	background: rgba(0, 0, 0, 0.61);
+	-webkit-border-radius: 100px;
+}
+
+ ::-webkit-scrollbar-thumb:vertical {
+	min-height: 10px;
+}
+
+ ::-webkit-scrollbar-thumb:horizontal {
+	min-width: 10px;
+}
+
+ ::-webkit-resizer {
+	background-color: transparent;
+}
+</style>
